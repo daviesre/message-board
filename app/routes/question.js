6544987f8ac9e8200c8ccb22a2f652e145db5d94
Russ Davies <russdavies392@gmail.com>
2016-09-03 14:55:51 -0700

    generate most files needed using ember

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params) {
+    return this.store.findRecord('question', params.question_id);
+  },
+
+  actions: {
+    save3(params) {
+      var newQuestion = this.store.createRecord('answer', params);
+      var question = params.question;
+      question.get('answers').addObject(newQuestion);
+      newQuestion.save().then(function() {
+        return question.save();
+      });
+      this.transitionTo('question', params.question);
+    },
+    destroyCity(question) {
+      var answer_deletions = question.get('answers').map(function(answer) {
+        return answer.destroyRecord();
+      });
+      Ember.RSVP.all(answer_deletions).then(function() {
+        return question.destroyRecord();
+      });
+      this.transitionTo('index');
+    }
+  }
 });
