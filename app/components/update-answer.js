7fe409e7f1a573fc0f7469a9dbfbc77ba9ff2818
Russ Davies <russdavies392@gmail.com>
2016-09-03 15:08:02 -0700

    add update-answer js and hbs

--- app/components/update-answer.js
+++ app/components/update-answer.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  updateAnswerForm: false,
+  actions: {
+    updateAnswerForm() {
+      this.set('updateAnswerForm', true);
+    },
+    update(answer) {
+      var params = {
+        owner: this.get('content'),
+        city: this.get('author'),
+      };
+      this.set('updateAnswerForm', false);
+      this.sendAction('update', answer, params);
+    }
+  }
 });
