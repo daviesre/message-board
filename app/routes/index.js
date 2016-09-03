d6c237b1c5ed344b91d9fa1478432c0fe05acba1
Russ Davies <russdavies392@gmail.com>
2016-09-03 14:23:09 -0700

    add index.js file

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model() {
+    return Ember.RSVP.hash({
+      questions: this.store.findAll('question'),
+      anwers: this.store.findAll('answer')
+    });
+  },
+  actions: {
+    save3(params) {
+      var newQuestion = this.store.createRecord('question', params);
+      newQuestion.save();
+      this.transitionTo('index');
+    },
+    saveCity(params) {
+      var newAnswer = this.store.createRecord('answers', params);
+      newAnswer.save();
+      this.transitionTo('index');
+    }
+  }
 });
