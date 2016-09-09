ecd61f8ff9bcd2fe888ce48fb68d79aef60d7bd5
Russ Davies <russdavies392@gmail.com>
2016-09-08 18:07:10 -0700

    correct typos

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -4,9 +4,9 @@ export function questionPopularity(params) {
   var question = params[0];
 
   if(question.get('answers').get('length') >= 2) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire">Hot!</span>')
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire">Hot!</span>');
   } else if(question.get('answers').get('length') === 0) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove">No</span>')
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove">No</span>');
   }
 }
 
