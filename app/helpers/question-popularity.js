a9a4836b91316d57a57d69ac6a614ce1e45aacd5
Russ Davies <russdavies392@gmail.com>
2016-09-08 17:50:28 -0700

    Add helper for popular and unpopular questions

---
+++ app/helpers/question-popularity.js
@@ -0,0 +1,13 @@
+import Ember from 'ember';
+
+export function questionPopularity(params) {
+  var question = params[0];
+
+  if(question.get('answers').get('length') >= 2) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire">Hot!</span>')
+  } else if(question.get('answers').get('length') === 0) {
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove"></span>')
+  }
+}
+
+export default Ember.Helper.helper(questionPopularity);
