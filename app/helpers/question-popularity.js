412047e71ae98454e0dac681ae515e245cde779b
Russ Davies <russdavies392@gmail.com>
2016-09-08 17:55:33 -0700

    add popular and unpopular questions helper

--- app/helpers/question-popularity.js
+++ app/helpers/question-popularity.js
@@ -6,7 +6,7 @@ export function questionPopularity(params) {
   if(question.get('answers').get('length') >= 2) {
     return Ember.String.htmlSafe('<span class="glyphicon glyphicon-fire">Hot!</span>')
   } else if(question.get('answers').get('length') === 0) {
-    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove"></span>')
+    return Ember.String.htmlSafe('<span class="glyphicon glyphicon-remove">No</span>')
   }
 }
 
