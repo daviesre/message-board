0ef33f2a107d56f76757a49bfe0776089130448e
Russ Davies <russdavies392@gmail.com>
2016-09-09 21:40:14 -0700

    add popup for favorites

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -2,9 +2,10 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   favoriteQuestion: Ember.inject.service(),
-  
+
   actions: {
     addToFavorites(item) {
+      alert("Successfully added to your favorites!");
       this.get('favoriteQuestion').add(item);
     },
   }
